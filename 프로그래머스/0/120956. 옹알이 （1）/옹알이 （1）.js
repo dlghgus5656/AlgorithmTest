6a742df857e66120f65110b063e2ce82acba6e36
Lee ho hyeon <82634597+dlghgus5656@users.noreply.github.com>
2025-03-26 14:26:48 +0900

    [level 0] Title: 옹알이 (1), Time: 0.32 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -0,0 +1,16 @@
+function solution(babbling) {
+var answer = 0;
+    
+  for (let word of babbling) {
+
+    
+    if (/(aya|ye|woo|ma)\1+/g.test(word)) continue;
+
+ 
+    if (/^(aya|ye|woo|ma)+$/g.test(word)) {
+      answer++;
+    }
+  }
+    
+    return answer;
+}

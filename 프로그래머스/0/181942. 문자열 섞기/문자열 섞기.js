bbc060e74093077eaa7d14e0c263e6395c29afaf
Lee ho hyeon <82634597+dlghgus5656@users.noreply.github.com>
2025-03-26 14:27:45 +0900

    Delete 프로그래머스/0/181942. 문자열 섞기 directory

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.js
+++
@@ -1,8 +0,0 @@
-function solution(str1, str2) {
-    let arrStr1 = str1.split('')
-    let arrStr2 = str2.split('')
-
-    var answer = arrStr1.map((arr, i) => arr + arrStr2[i]).join('');
-    
-    return answer;
-}

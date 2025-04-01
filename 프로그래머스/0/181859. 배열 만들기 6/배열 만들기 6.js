9c6968e221c6b4d70b61b5bcdb2eedc60f22fd8c
Lee ho hyeon <82634597+dlghgus5656@users.noreply.github.com>
2025-04-01 19:11:26 +0900

    [level 0] Title: 배열 만들기 6, Time: 10.08 ms, Memory: 63.6 MB -BaekjoonHub

--- 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.js
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.js
@@ -1,14 +1,33 @@
-function solution(arr) {
-    const answer = arr.reduce((acc, cur, i) => {
-        if(acc === []) {
-            acc.push(cur)
-        } else if(acc[acc.length - 1] === cur){
-            acc.pop()
-        } else {
-            acc.push(cur)
-        }
-        return acc
-    },[])
+// function solution(arr) {
+//     const answer = arr.reduce((acc, cur, i) => {
+//         if(acc === []) {
+//             acc.push(cur)
+//         } else if(acc[acc.length - 1] === cur){
+//             acc.pop()
+//         } else {
+//             acc.push(cur)
+//         }
+//         return acc
+//     },[])
     
-    return answer.length === 0 ? [-1] : answer;
+//     return answer.length === 0 ? [-1] : answer;
+// }
+function solution(arr) {
+    const stk = [];
+    let i = 0;
+
+    while (i < arr.length) {
+        if (stk.length === 0) {
+            stk.push(arr[i]);
+            i++;
+        } else if (stk[stk.length - 1] === arr[i]) {
+            stk.pop();
+            i++;
+        } else {
+            stk.push(arr[i]);
+            i++;
+        }
+    }
+
+    return stk.length === 0 ? [-1] : stk;
 }

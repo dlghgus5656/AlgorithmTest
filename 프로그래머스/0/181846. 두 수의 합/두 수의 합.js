502b17d58f280cc405cbb94a9c44fc9998ce5b01
Lee ho hyeon <82634597+dlghgus5656@users.noreply.github.com>
2025-04-04 17:12:04 +0900

    [level 0] Title: 두 수의 합, Time: 13.62 ms, Memory: 41.9 MB -BaekjoonHub

--- 프로그래머스/0/181846. 두 수의 합/두 수의 합.js
+++ 프로그래머스/0/181846. 두 수의 합/두 수의 합.js
@@ -1,3 +1,40 @@
+// function solution(a, b) {
+//     return (String(BigInt(a) + BigInt(b)));
+// }
 function solution(a, b) {
-    return (String(BigInt(a) + BigInt(b)));
+    a = [...a];
+    b = [...b];
+    let answer = [];
+
+    let next = 0;
+    while(a.length && b.length) {
+        let _a = +a.pop();
+        let _b = +b.pop();
+        let sum = _a + _b + next;
+
+        if(sum >= 10) {
+            sum -= 10;
+            next = 1;
+        } else
+            next = 0;
+
+        answer.push(sum);
+    }
+
+    let rest = a.length ? a : b;
+    while(rest.length) {
+        let sum = +rest.pop() + next;
+        if(sum >= 10) {
+            sum -= 10;
+            next = 1;
+} else
+            next = 0;
+
+        answer.push(sum);
+    }
+
+    if(next)
+        answer.push(next);
+
+    return answer.reverse().join("");
 }

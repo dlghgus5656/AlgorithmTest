bd287b6d64c70c3044520aa72c7c25ffb8ade936
Lee ho hyeon <82634597+dlghgus5656@users.noreply.github.com>
2025-04-08 17:26:54 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 0.25 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
@@ -1,15 +1,24 @@
-function solution(n) {
-    let answer = [];
+// function solution(n) {
+//     let answer = [];
     
-    for(let i = 0; i < n; i++) {
-         let numArr = []
+//     for(let i = 0; i < n; i++) {
+//          let numArr = []
         
-        for(let j = 0; j < n; j++){
-            numArr.push(
-            i === j ? 1 : 0)
-        }
-        answer.push(numArr)
-    }
+//         for(let j = 0; j < n; j++){
+//             numArr.push(
+//             i === j ? 1 : 0)
+//         }
+//         answer.push(numArr)
+//     }
     
+//     return answer;
+// }
+function solution(n) {
+    const answer = new Array(n);
+    for (let i = 0; i < n; i++) {
+        const row = new Array(n).fill(0);
+        row[i] = 1;
+        answer[i] = row;
+    }
     return answer;
 }

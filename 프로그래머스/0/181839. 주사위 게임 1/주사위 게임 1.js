00560822fa740f59bec83460dc998ba4197b10f3
Lee ho hyeon <82634597+dlghgus5656@users.noreply.github.com>
2025-04-08 15:39:21 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.js
@@ -0,0 +1,12 @@
+function solution(a, b) {
+    const isAEven = a % 2 === 0;
+    const isBEven = b % 2 === 0;
+
+    if (isAEven && isBEven) {
+        return Math.abs(a - b);
+    } else if (!isAEven && !isBEven) {
+        return a ** 2 + b ** 2;
+    } else {
+        return 2 * (a + b);
+    }
+}

76566f1a3435c3ca51d32b18814fe83d04c5d702
Lee ho hyeon <82634597+dlghgus5656@users.noreply.github.com>
2025-04-08 17:09:29 +0900

    [level 0] Title: l로 만들기, Time: 12.97 ms, Memory: 39.2 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.js
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.js
@@ -1,4 +1,5 @@
 function solution(myString) {
+    // const result = []
     const result = new Array(myString.length);
     for (let i = 0; i < myString.length; i++) {
         // result.push(myString[i] < 'l' ? 'l' : myString[i]);
@@ -8,7 +9,27 @@ function solution(myString) {
 }
 
 // function solution(myString) {
+//     const result = new Array(myString.length);
+//     const lCode = 'l'.charCodeAt(0);
+
+//     for (let i = 0; i < myString.length; i++) {
+//         const code = myString.charCodeAt(i);
+//         result[i] = code < lCode ? 'l' : myString[i];
+//     }
+
+//     return result.join('');
+// }
+
+// function solution(myString) {
+//     var answer = [...myString].map((str) => str.charCodeAt() < "l".charCodeAt() ? "l" : str ).join("");
+//     return answer;
+// }
+
+// function solution(myString) {
 //     return [...myString]
 //         .map(char => char < 'l' ? 'l' : char)
 //         .join('');
 // }
+
+
+

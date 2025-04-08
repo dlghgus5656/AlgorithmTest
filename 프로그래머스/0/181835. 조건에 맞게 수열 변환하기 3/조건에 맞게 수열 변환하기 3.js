6621a589a187cbe287c405e9c2cd388365712eba
Lee ho hyeon <82634597+dlghgus5656@users.noreply.github.com>
2025-04-08 16:33:12 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 3, Time: 8.65 ms, Memory: 121 MB -BaekjoonHub

--- 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.js
+++ 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.js
@@ -1,15 +1,32 @@
 function solution(arr, k) {
-    const answer = arr
-    
-   for(let i = 0; i < answer.length; i++) {
-       if(k % 2 !== 0) {
-           answer[i] *= k
-       } else {
-           answer[i] += k
-       }
-   }
-    return answer;
+    const result = new Array(arr.length); // 미리 배열 크기 확보
+
+    if (k % 2 === 1) {
+        for (let i = 0; i < arr.length; i++) {
+            result[i] = arr[i] * k;
+        }
+    } else {
+        for (let i = 0; i < arr.length; i++) {
+            result[i] = arr[i] + k;
+        }
+    }
+
+    return result;
 }
+
+// function solution(arr, k) {
+//     const answer = arr
+    
+//    for(let i = 0; i < answer.length; i++) {
+//        if(k % 2 !== 0) {
+//            answer[i] *= k
+//        } else {
+//            answer[i] += k
+//        }
+//    }
+//     return answer;
+// }
+
 
 // function solution(arr, k) {
 //     const answer = arr.map((num) => k % 2 === 0 ? num + k : num * k)

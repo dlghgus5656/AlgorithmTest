6d8147abc29b43734fce4cd2337a7c7c106cdf55
Lee ho hyeon <82634597+dlghgus5656@users.noreply.github.com>
2025-04-08 19:18:05 +0900

    [level 0] Title: 이차원 배열 대각선 순회하기, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181829. 이차원 배열 대각선 순회하기/이차원 배열 대각선 순회하기.js
+++ 프로그래머스/0/181829. 이차원 배열 대각선 순회하기/이차원 배열 대각선 순회하기.js
@@ -1,11 +1,26 @@
-function solution(board, k) {
-    let answer = 0;
+// function solution(board, k) {
+//     let sum = 0;
     
-    for(let i = 0; i < board.length; i++) {
-        for(let j = 0; j < board[i].length; j++) {
-            if(i + j <= k) answer += board[i][j]
+//     for(let i = 0; i < board.length; i++) {
+//         for(let j = 0; j < board[i].length; j++) {
+//             if(i + j <= k) sum += board[i][j]
+//         }
+//     }
+    
+//     return sum;
+// }
+
+function solution(board, k) {
+    let sum = 0;
+    const rowCount = board.length;
+    const colCount = board[0].length;
+
+    for (let i = 0; i < rowCount; i++) {
+        const maxJ = Math.min(colCount - 1, k - i);
+        for (let j = 0; j <= maxJ; j++) {
+            sum += board[i][j];
         }
     }
-    
-    return answer;
+
+    return sum;
 }

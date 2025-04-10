e53cf3496819c5d3f7b790f42738c94682d0b6e6
Lee ho hyeon <82634597+dlghgus5656@users.noreply.github.com>
2025-04-10 17:14:10 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.28 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.js
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.js
@@ -1,59 +1,19 @@
 function solution(n, w, num) {
-    let map = [];
-    let temp = [];
-    let direction = true;
-    for (let i = 1; i <= n; ++i) {
-        if (direction) {
-            temp.push(i);
-        } else {
-            temp.unshift(i);
-        }
-        if (i === n) {
-            map.push(temp);
-            break;
-        }
-        if (parseInt(i % w) === 0) {
-            map.push(temp);
-            temp = [];
-            direction = !direction;
-        }
+    if (w === 1) return n - num + 1;
+
+    const arr = Array.from({length: w}, () => []);
+    const h = Math.trunc(n / w);
+    for (let i = 1; i <= n; i++) {
+        const r = i % w;
+        if (!r) arr[w - 1].push(i)
+        else arr[r - 1].push(i);
+        if (!r) arr.reverse();
     }
-
-    if (map[map.length - 1].length !== w) {
-        let itemCnt = map[map.length - 1].length;
-
-        if (direction) {
-            let newLine = map[map.length - 1].concat(Array(w - itemCnt).fill(0));
-            map[map.length - 1] = newLine;
-        } else {
-            let newLine = Array(w - itemCnt)
-                .fill(0)
-                .concat([...map[map.length - 1]]);
-            map[map.length - 1] = newLine;
-        }
+    
+    if (h % 2) arr.reverse();
+    
+    for (let i = 0; i < w; i++) {
+        if (arr[i].includes(num)) return arr[i].length - arr[i].indexOf(num);
     }
-
-    let answer = 0;
-
-    let row = 0,
-        column = 0;
-    for (let r = 0; r < map.length; ++r) {
-        for (let c = 0; c < w; ++c) {
-            if (map[r][c] === num) {
-                row = r;
-                column = c;
-            }
-        }
-    }
-
-    while (1) {
-        if (map[row] && map[row][column]) {
-            ++answer;
-            ++row;
-        } else {
-            break;
-        }
-    }
-
-    return answer;
+    
 }

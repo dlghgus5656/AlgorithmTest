b366417aab546c134f39a9c04456195006c07484
Lee ho hyeon <82634597+dlghgus5656@users.noreply.github.com>
2025-05-04 18:38:45 +0900

    [level 1] Title: 덧칠하기, Time: 148.98 ms, Memory: 43.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
@@ -0,0 +1,19 @@
+function solution(n, m, section) {
+    let count = 0;    // 최소 롤러 횟수
+    let index = 0;    // section에서 아직 칠하지 않은 첫 위치
+
+    while (index < section.length) {
+        console.log(index)
+        const start = section[index];  // 현재 칠해야 할 시작점
+        const end = start + m - 1;     // 현재 롤러로 덮을 끝 점
+
+        count++;                       // 롤러 한 번 사용 + 1
+
+        // 롤러로 덮은 영역의 다음 칠해야 할 지점까지 스킵
+        while (index < section.length && section[index] <= end) {
+            index++;
+        }
+    }
+
+    return count;
+}

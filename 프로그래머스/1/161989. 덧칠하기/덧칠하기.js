543291e85ecf87104fc2a32d3c409599d2206b93
Lee ho hyeon <82634597+dlghgus5656@users.noreply.github.com>
2025-05-04 18:41:10 +0900

    [level 1] Title: 덧칠하기, Time: 28.83 ms, Memory: 39 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
@@ -1,19 +1,30 @@
-function solution(n, m, section) {
-    let count = 0;    // 최소 롤러 횟수
-    let index = 0;    // section에서 아직 칠하지 않은 첫 위치
+// function solution(n, m, section) {
+//     let count = 0;    // 최소 롤러 횟수
+//     let index = 0;    // section에서 아직 칠하지 않은 첫 위치
 
-    while (index < section.length) {
-        console.log(index)
-        const start = section[index];  // 현재 칠해야 할 시작점
-        const end = start + m - 1;     // 현재 롤러로 덮을 끝 점
+//     while (index < section.length) {
+//         console.log(index)
+//         const start = section[index];  // 현재 칠해야 할 시작점
+//         const end = start + m - 1;     // 현재 롤러로 덮을 끝 점
 
-        count++;                       // 롤러 한 번 사용 + 1
+//         count++;                       // 롤러 한 번 사용 + 1
 
-        // 롤러로 덮은 영역의 다음 칠해야 할 지점까지 스킵
-        while (index < section.length && section[index] <= end) {
-            index++;
+//         // 롤러로 덮은 영역의 다음 칠해야 할 지점까지 스킵
+//         while (index < section.length && section[index] <= end) {
+//             index++;
+//         }
+//     }
+
+//     return count;
+// }
+function solution(n, m, sections) {
+    var answer = 0;
+    var painted = 0;
+    for(var section of sections) {
+        if(painted < section) {
+            answer++;
+            painted = section+m-1;
         }
     }
-
-    return count;
+    return answer;
 }

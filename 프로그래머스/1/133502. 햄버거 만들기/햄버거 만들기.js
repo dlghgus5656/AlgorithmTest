2a54b5b4a77a5c4491d0b7a365d30373fb3c838a
Lee ho hyeon <82634597+dlghgus5656@users.noreply.github.com>
2025-06-18 15:55:50 +0900

    [level 1] Title: 햄버거 만들기, Time: 53.85 ms, Memory: 91 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
@@ -1,22 +1,40 @@
-function solution(ingredient) {
-    const stack = [] // 스택 활용
-    const answerarr = [1, 2, 3, 1]
-    let count = 0 // 포장 햄버거 수
+// function solution(ingredient) {
+//     const stack = [] // 스택 활용
+//     const answerarr = [1, 2, 3, 1]
+//     let count = 0 // 포장 햄버거 수
         
-    for (let i = 0; i < ingredient.length; i++) {
-        stack.push(ingredient[i])
-        let length = 4
+//     for (let i = 0; i < ingredient.length; i++) {
+//         stack.push(ingredient[i])
+//         let length = 4
         
-        if (stack.length >= 4) {
-            const lastFour = stack.slice(-4);
+//         if (stack.length >= 4) {
+//             const lastFour = stack.slice(-4);
             
-            if (lastFour.every((num, i) => num === answerarr[i])) { // 햄버거 재료 순서 일치한다면
-                    stack.splice(stack.length - 4, 4)
-                    count++
-                }
+//             if (lastFour.every((num, i) => num === answerarr[i])) { // 햄버거 재료 순서 일치한다면
+//                     stack.splice(stack.length - 4, 4)
+//                     count++
+//                 }
+//         }
+//     }
+    
+    
+//     return count;
+// }
+
+function solution(ingredient) {
+    let stk = [];
+    let count = 0;
+    for (let i = 0; i < ingredient.length; i++) {
+        stk.push(ingredient[i]);
+        if (
+            stk[stk.length-1] === 1 &&
+            stk[stk.length-2] === 3 &&
+            stk[stk.length-3] === 2 &&
+            stk[stk.length-4] === 1
+        ) {
+            count++;
+            stk.splice(-4);
         }
     }
-    
-    
     return count;
 }

bb8102187400fe7c7179889044a28cbafd36cf1f
Lee ho hyeon <82634597+dlghgus5656@users.noreply.github.com>
2025-06-18 15:53:55 +0900

    [level 1] Title: 햄버거 만들기, Time: 95.34 ms, Memory: 97.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
@@ -0,0 +1,22 @@
+function solution(ingredient) {
+    const stack = [] // 스택 활용
+    const answerarr = [1, 2, 3, 1]
+    let count = 0 // 포장 햄버거 수
+        
+    for (let i = 0; i < ingredient.length; i++) {
+        stack.push(ingredient[i])
+        let length = 4
+        
+        if (stack.length >= 4) {
+            const lastFour = stack.slice(-4);
+            
+            if (lastFour.every((num, i) => num === answerarr[i])) { // 햄버거 재료 순서 일치한다면
+                    stack.splice(stack.length - 4, 4)
+                    count++
+                }
+        }
+    }
+    
+    
+    return count;
+}

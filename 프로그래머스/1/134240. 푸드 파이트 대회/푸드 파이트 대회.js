718a1826ca6d937b30fa5b2e5b1110a680174c1c
Lee ho hyeon <82634597+dlghgus5656@users.noreply.github.com>
2025-06-18 14:28:52 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.87 ms, Memory: 34.1 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -9,15 +9,14 @@ function solution(food) {
         while (count < foodNum) {
             result.push(i)
             count++
-
         }
-        
     }
     const reverse = result.slice().reverse() // 원본 배열 유지용 slice
     
     return [...result, 0,...reverse].join("");
 }
 
-// 0번 인덱스는 물 즉 1번 인덱스부터 음식 시작
-// 음식을 2로 나눈 몫만큼 두명에게 좌우 분배
-// 마지막 음식까지 분배후 가운데는 0
+// 0번 인덱스는 물 (항상 중앙)
+// 1번부터는 음식이며, 각 음식은 2로 나눈 몫만큼 양쪽 선수에게 배분
+// 음식은 번호순으로 나열하고, 좌우 대칭을 이루도록 배열 구성
+// 최종 결과는: [왼쪽 음식들] + 0(물) + [오른쪽 음식들 (역순)]

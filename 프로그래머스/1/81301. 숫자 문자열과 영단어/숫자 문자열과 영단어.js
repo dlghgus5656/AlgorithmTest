798b0b1b56368f1ccbb14e12ec4a3c925cb408f7
Lee ho hyeon <82634597+dlghgus5656@users.noreply.github.com>
2025-06-26 16:24:39 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -0,0 +1,35 @@
+function solution(s) {
+    const map = new Map([
+        ["zero", 0], // 키-값 쌍의 배열
+        ["one", 1],
+        ["two", 2],
+        ["three", 3],
+        ["four", 4],
+        ["five", 5],
+        ["six", 6],
+        ["seven", 7],
+        ["eight", 8],
+        ["nine", 9],
+    ]);
+    
+    let answer = [];
+    let word = ""
+    
+    for (const abc of s) {
+        
+        if (Number(abc) || abc === "0") {
+            answer.push(abc)
+        } 
+        else {
+            word += abc
+        }
+
+        if (map.has(word)) {
+            answer.push(map.get(word))
+            word = ""
+        }
+        
+    }
+    
+    return Number(answer.join(""));
+}

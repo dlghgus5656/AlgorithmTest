8fa6daa262761ea5ed3673a5f1499b83e76eb1f3
Lee ho hyeon <82634597+dlghgus5656@users.noreply.github.com>
2025-06-26 16:40:22 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,4 +1,4 @@
-function solution(s) {
+// function solution(s) {
 //     const map = new Map([
 //         ["zero", 0], // 키-값 쌍의 배열
 //         ["one", 1],
@@ -30,16 +30,26 @@ function solution(s) {
 //         }
         
 //     }
-        let numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-    var answer = s;
 
-    for(let i=0; i< numbers.length; i++) {
-        let arr = answer.split(numbers[i]);
-                console.log(arr)
-// console.log(arr.join(), "arr")
-        answer = arr.join(i);
-    }
-    
-    return Number(answer)
-    // return Number(answer.join(""));
+//     return Number(answer.join(""));
+// }
+function solution(s) {
+  const map = {
+    zero: "0",
+    one: "1",
+    two: "2",
+    three: "3",
+    four: "4",
+    five: "5",
+    six: "6",
+    seven: "7",
+    eight: "8",
+    nine: "9",
+  };
+
+  for (const [word, digit] of Object.entries(map)) {
+    s = s.replaceAll(word, digit);
+  }
+
+  return Number(s);
 }

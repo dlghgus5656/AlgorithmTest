d21066d3027ea87886370cc5d198ff4a37c4a84f
Lee ho hyeon <82634597+dlghgus5656@users.noreply.github.com>
2025-06-26 16:31:34 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 3.69 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,35 +1,45 @@
 function solution(s) {
-    const map = new Map([
-        ["zero", 0], // 키-값 쌍의 배열
-        ["one", 1],
-        ["two", 2],
-        ["three", 3],
-        ["four", 4],
-        ["five", 5],
-        ["six", 6],
-        ["seven", 7],
-        ["eight", 8],
-        ["nine", 9],
-    ]);
+//     const map = new Map([
+//         ["zero", 0], // 키-값 쌍의 배열
+//         ["one", 1],
+//         ["two", 2],
+//         ["three", 3],
+//         ["four", 4],
+//         ["five", 5],
+//         ["six", 6],
+//         ["seven", 7],
+//         ["eight", 8],
+//         ["nine", 9],
+//     ]);
     
-    let answer = [];
-    let word = ""
+//     let answer = [];
+//     let word = ""
     
-    for (const abc of s) {
+//     for (const abc of s) {
         
-        if (Number(abc) || abc === "0") {
-            answer.push(abc)
-        } 
-        else {
-            word += abc
-        }
+//         if (Number(abc) || abc === "0") {
+//             answer.push(abc)
+//         } 
+//         else {
+//             word += abc
+//         }
 
-        if (map.has(word)) {
-            answer.push(map.get(word))
-            word = ""
-        }
+//         if (map.has(word)) {
+//             answer.push(map.get(word))
+//             word = ""
+//         }
         
+//     }
+        let numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    var answer = s;
+
+    for(let i=0; i< numbers.length; i++) {
+        let arr = answer.split(numbers[i]);
+                console.log(arr)
+// console.log(arr.join(), "arr")
+        answer = arr.join(i);
     }
     
-    return Number(answer.join(""));
+    return Number(answer)
+    // return Number(answer.join(""));
 }

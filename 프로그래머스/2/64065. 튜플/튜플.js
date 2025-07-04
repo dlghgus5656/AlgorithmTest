a7f244de96a72ed8aa7efc5b39c128e4b2d94031
Lee ho hyeon <82634597+dlghgus5656@users.noreply.github.com>
2025-07-04 14:53:42 +0900

    [level 2] Title: 튜플, Time: 24.68 ms, Memory: 46.2 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.js
+++ 프로그래머스/2/64065. 튜플/튜플.js
@@ -1,14 +1,19 @@
 function solution(s) {
     const answer = []
-    const groups = s.slice(2, -2).split("},{");
-    const sarr = groups.map(item => item.split(",")).sort((a, b) => a.length - b.length)
+    const check = new Set(); // 큰 입력 대비 Set
+    const groups = s.slice(2, -2).split("},{"); // 튜플 구조 -> 배열 변환
+    const sarr = groups.map(item => item.split(",")).sort((a, b) => a.length - b.length); // 이중 배열구조, 배열길이 오름차순 정렬
     
     
     for (const numbers of sarr) {
-        for (const num of numbers) {
-            if (!answer.includes(num)) answer.push(num)
+        for (const num of numbers) { // answer에 포함되지 않은 수 하나씩 push
+            if (!check.has(num)) { // includes보다 set.has 큰 입력 대비 성능 up
+                check.add(num);
+                answer.push(num);
+            }
         }
     }
     
     return answer.map(Number);
 }
+

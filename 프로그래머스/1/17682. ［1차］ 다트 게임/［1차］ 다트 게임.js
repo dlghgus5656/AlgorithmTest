807c9c7d66e783c1302256cf24f6e3dd48e782e7
Lee ho hyeon <82634597+dlghgus5656@users.noreply.github.com>
2025-07-14 17:46:33 +0900

    [level 1] Title: [1차] 다트 게임, Time: 4.65 ms, Memory: 32.3 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
@@ -1,23 +1,104 @@
-function solution(dartResult) {
-    const regex = /(\d{1,2})([SDT])([*#]?)/g;
-    const matches = [...dartResult.matchAll(regex)]; // ex) ['2D*', '2', 'D', '*']
+// function solution(dartResult) {
+//     const regex = /(\d{1,2})([SDT])([*#]?)/g;
+//     const matches = [...dartResult.matchAll(regex)]; // ex) ['2D*', '2', 'D', '*']
 
-    const answer = [];
+//     const answer = [];
 
-    matches.forEach(([_, num, bonus, option]) => {
-        num = Number(num);
-        const pow = bonus === 'S' ? 1 : bonus === 'D' ? 2 : 3;
-        let score = num ** pow;
+//     matches.forEach(([_, num, bonus, option]) => {
+//         num = Number(num);
+//         const pow = bonus === 'S' ? 1 : bonus === 'D' ? 2 : 3;
+//         let score = num ** pow;
 
-        if (option === '*') {
-            if (answer.length > 0) answer[answer.length - 1] *= 2; // 이전 점수도 *2
-            score *= 2;
-        } else if (option === '#') {
-            score *= -1;
+//         if (option === '*') {
+//             if (answer.length > 0) answer[answer.length - 1] *= 2; // 이전 점수도 *2
+//             score *= 2;
+//         } else if (option === '#') {
+//             score *= -1;
+//         }
+
+//         answer.push(score);
+//     });
+
+//     return answer.reduce((a, b) => a + b, 0);
+// }
+function singlePoint(stack) {
+    if (stack.length > 0) {
+        const point = Math.pow(stack.pop(), 1)
+        stack.push(point)
+
+        return true
+    }
+    return false
+}
+function doublePoint(stack) {
+    if (stack.length > 0) {
+        const point = Math.pow(stack.pop(), 2)
+        stack.push(point)
+
+        return true
+    }
+    return false
+}
+function triplePoint(stack) {
+    if (stack.length > 0) {
+        const point = Math.pow(stack.pop(), 3)
+        stack.push(point)
+
+        return true
+    }
+    return false
+}
+function star(stack) {
+    if (stack.length > 0){
+        const point = stack.pop() * 2
+
+        if (stack.length >= 1) {
+            const prevPoint = stack.pop() * 2
+            stack.push(prevPoint)
         }
 
-        answer.push(score);
-    });
+        stack.push(point)
 
-    return answer.reduce((a, b) => a + b, 0);
+        return true
+    }
+    return false
+}
+function acha(stack) {
+    if (stack.length > 0) {
+        const point = stack.pop() * -1
+        stack.push(point)
+        return true
+    }
+    return false
+}
+
+const operators = {
+    'S': singlePoint,
+    'D': doublePoint,
+    'T': triplePoint,
+    '*': star,
+    '#': acha
+}
+function isOperator(character) {
+    return operators[character]
+}
+function solution(dartResult) {
+    var answer = 0
+    const stack = []
+
+    for (let i = 0; i < dartResult.length; i++) {
+
+        const number = parseInt(dartResult.slice(i))
+
+        if (Number.isInteger(number)) {
+            stack.push(number)
+            i += (number.toString().length - 1)
+        } else {
+            const operator  = isOperator(dartResult.charAt(i))
+            operator(stack)
+        }
+    }
+    console.log(stack)
+    answer = stack.reduce((accumulator, currentValue) => accumulator + currentValue)
+    return answer
 }

0d44c17808526ba58049532bd75139d8eff3e9f4
Lee ho hyeon <82634597+dlghgus5656@users.noreply.github.com>
2025-08-28 16:04:35 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.27 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/README.md
+++ 프로그래머스/2/389479. 서버 증설 횟수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 서버 증설 횟수 - 389479 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/389479?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/389479) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.28 ms
+메모리: 33.4 MB, 시간: 0.27 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 28일 16:01:18
+2025년 08월 28일 16:04:33
 
 ### 문제 설명
 
--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.js
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.js
@@ -3,19 +3,20 @@ function solution(players, m, k) {
     
     let nowSever = new Map();
     let sumSever = 0;
+    
     for (let i = 0; i < players.length; i++) {
         const player = players[i]
         
-        if (nowSever.has(i)) nowSever.delete(i)
+        if (nowSever.has(i)) nowSever.delete(i) // 종료 시작 서버 제거
         
-        const nowSeverSize = [...nowSever.values()].reduce((a, b) => a + b, 0);
+        const nowSeverSize = [...nowSever.values()].reduce((a, b) => a + b, 0); // 현재 증설된 서버의 수
 
         
         if (player >= m) {
-            const severOn = Math.trunc(player / m) - nowSeverSize
+            const severOn = Math.trunc(player / m) - nowSeverSize // 증설해야할 서버 수 음수면 새로 증설 x
             if (severOn > 0) {
-                nowSever.set(i + k,  severOn)
-                sumSever += severOn
+                nowSever.set(i + k,  severOn) //종료시각을 키값 : 증설된 서버 수 벨류값
+                sumSever += severOn // 누적 증설 횟수
             }
         }
     }

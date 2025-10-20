503eb97416f4f1e8c23f43f0a0c42fae933ee489
Lee ho hyeon <82634597+dlghgus5656@users.noreply.github.com>
2025-10-20 19:06:18 +0900

    [level 1] Title: 폰켓몬, Time: 1.24 ms, Memory: 34.5 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,14 +1,15 @@
 function solution(nums) {
     const N = 2;
     
-    const setNums = new Set(nums)
+    const setNums = new Set(nums) // 중복 포켓몬 제거
     const result = []
     
     if (setNums.size === 1) return 1; // 같은 종류의 포켓몬만 있을시
     
+    
     for (const num of setNums) {
         if(result.length < Math.floor(nums.length / N)) {
-            if (!result.includes(num)) result.push(num)
+             result.push(num)
         }
         else break;   
     }

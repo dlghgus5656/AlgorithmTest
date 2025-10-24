7988f6e33543ca949f338c08c571ce62bcf372f3
Lee ho hyeon <82634597+dlghgus5656@users.noreply.github.com>
2025-10-24 19:30:32 +0900

    [level 2] Title: 기능개발, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -3,17 +3,13 @@ function solution(progresses, speeds) {
     
     const day = [];
     
-    for (let i = 0; i < progresses.length; i++) {
-        let progres = progresses[i];
-        const speed = speeds[i];
-        
-        let index = 0;
-        while (progres < 100) {
-            progres += speed
-            index++
-        }
-        day.push(index)
+    // 각 작업의 완료까지 걸리는 일수 계산
+    for (let i = 0; i < progresses.length; i++) { // Math.ceil(remain / speeds[i]); 수학 계산으로 구하기
+        const remain = 100 - progresses[i];
+        const daysNeeded = Math.ceil(remain / speeds[i]);
+        day.push(daysNeeded);
     }
+
     
     let currentDay = day[0];
     let count = 1;
@@ -33,12 +29,3 @@ function solution(progresses, speeds) {
 
     return answer;
 }
-
-
-// 100% 일때 배포
-// 순서는 먼저 완료된 순으로 먼저 개발되지만 배포는 뒤에있는 기능이 앞에 배포될때 같이 배포 - 이 순서는 상관 없음
-// 하루에 몇개의 기능이 배포되는지 수를 배열로 각 날에 배포수를 리턴
-
-// 반복문 돌면서 프로그래스 인덱스와 스피스 인덱스를 활용해서 계속 더해준다
-// 더한 값이 100이상이 되면 완료 상태
-// 앞에 인덱스가 완료 상태라면 뒤에 완료된 모든 기능을 배포한다 = 배포 수를 리턴에 푸쉬
